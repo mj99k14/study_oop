15ea4b736c63ee06fc4e2248fae5c1c6d16b56b5
mj99k14 <ekfkawnl644@gmail.com>
2025-09-05 19:17:01 +0900

    9/5 수업

---
+++ dict_pr.py
@@ -0,0 +1,55 @@
+# class Bar:
+#     name = "kmj"
+
+#     def __init__(self):
+#         self.age = 20
+        
+#     def hello(self):
+#         print(self.name)
+
+
+
+# obj = Bar()
+# obj.hello()
+
+# class Bar:
+#     category = "animal"
+
+#     def __init__(self, name):
+#         self.name = name
+    
+#     def hello(self):
+#         print("Hello,", self.name)
+
+
+# obj1 = Bar("dog")
+# obj2 = Bar("cat")
+
+# class Foo:
+#     univ = "YJU"
+    
+#     def __init__(self, name, id):
+#         self.name = name
+#         self.id = id
+        
+#     def prt_info(self):
+#         self.age = 20
+#         print(self.id, self.name, self.age)
+        
+# obj = Foo()
+# obj.prt_info()
+class Foo:
+    univ = "YJU"
+    dept = "GSC"
+    
+    def __init__(self, name):
+        self.name = name
+        
+    def prt_info(self, age):
+        self.age = age
+        print(self.name, self.age)
+        
+obj1 = Foo("kim")
+obj1.prt_info(20)
+obj2 = Foo("Hong")
+obj2.prt_info(30)
--- main.java
+++ main.java
@@ -1,24 +1,23 @@
-class Bar {
-    void  prt_name(){
-        System.out.println("Bar");
-    }
-}
+// class Bar {
+// void prt_name(){
+// System.out.println("Bar");
+// }
+// }
 
-class Foo{
-    void prt_name(){
-          System.out.println("Bar");
-    }
-}
+// class Foo{
+// void prt_name(){
+// System.out.println("Bar");
+// }
+// }
 
-
-public class main {
-    public static void prt(Foo obj){
-        obj.prt_name();
-    }
-    public static void main(String[] args) {
-        Bar bar = new Bar();
-        Foo foo = new Foo();
-        bar.prt_name();
-        foo.prt_name();
-    }
-}
+// public class main {
+// public static void prt(Foo obj){
+// obj.prt_name();
+// }
+// public static void main(String[] args) {
+// Bar bar = new Bar();
+// Foo foo = new Foo();
+// bar.prt_name();
+// foo.prt_name();
+// }
+// }
