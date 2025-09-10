2b7ff951ed9015dc7f6583f1a2a3c06b5f3ea98e
mj99k14 <ekfkawnl644@gmail.com>
2025-09-10 23:29:08 +0900

    수업 복습

--- dict_pr.py
+++ dict_pr.py
@@ -38,7 +38,23 @@
         
 # obj = Foo()
 # obj.prt_info()
-class Foo:
+# class Foo:
+#     univ = "YJU"
+#     dept = "GSC"
+    
+#     def __init__(self, name):
+#         self.name = name
+        
+#     def prt_info(self, age):
+#         self.age = age
+#         print(self.name, self.age)
+        
+# obj1 = Foo("kim")
+# obj1.prt_info(20)
+# obj2 = Foo("Hong")
+# obj2.prt_info(30)
+
+class bar:
     univ = "YJU"
     dept = "GSC"
     
@@ -49,7 +65,7 @@ class Foo:
         self.age = age
         print(self.name, self.age)
         
-obj1 = Foo("kim")
+obj1 = bar("kim")
 obj1.prt_info(20)
-obj2 = Foo("Hong")
+obj2 = bar("Hong")
 obj2.prt_info(30)
