2e49ddbe638b77fa239fcfefb2597869c5dd7954
mj99k14 <ekfkawnl644@gmail.com>
2025-09-11 10:48:30 +0900

    9/11맴버 매서드수업

--- python.py
+++ python.py
@@ -1,10 +1,37 @@
+# class Foo:
+#     #class member variables
+#     name ="Class object"
+#     count = 0
+    
+#     def __init__(self):
+#     #instance member variables
+#         self.count = 1
+    
+# obj = Foo()
+
+# print(obj.count)
+# print(Foo.count)
+
+
+
 class Foo:
-    name ="Class object"
-
-
+    # class member variables
+    class_var = 1
+    
+    def __init__(self):
+        # Instance member variables
+        self.instance_var = 2
+    
+    # class method
+    @classmethod
+    def class_method(cls, age):
+        cls.age = age
+    
+    # instance method
+    def instance_method(self, age):
+        self.age = age
+        
 obj = Foo()
-
-obj.name = "Foo instance"
-
-print(obj.name)
-print(Foo.name)
+obj.class_method(30)
+obj.instance_method(100)
+print(obj.age, Foo.age)
