d8b226d0720ab931a967650117cb9dad42dec47b
mj99k14 <ekfkawnl644@gmail.com>
2025-09-19 19:17:23 +0900

    mro설명

---
+++ python/0919mro.py
@@ -0,0 +1,44 @@
+# class A:
+#     def __init__(self):
+#         print("A")
+
+# class B(A):
+#        def __init__(self):
+#         print("B")
+
+
+# class C(A):
+#     def __init__(self):
+#         print("C")
+
+# class D(C,B):
+#     def __init__(self):
+#         print("D")
+#         super().__init__()
+
+
+# obj = D()
+
+
+# class Parent:
+#     def prt(self):
+#         print("Parent")
+# class Child(Parent):
+#     def prt(self):
+#         print("child")
+    
+    
+# obj = Child()
+# obj.prt()
+
+    
+class A(object):
+    N
+class B(A):
+    pass
+
+class C(B):
+    pass
+
+
+    
