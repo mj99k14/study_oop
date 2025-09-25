9750710907b52a160e6e599be91572696facbb2b
mj99k14 <ekfkawnl644@gmail.com>
2025-09-25 10:35:34 +0900

    수업

--- python/0925.py
+++ python/0925.py
@@ -1,3 +1,24 @@
+# class A:
+#     def __init__(self):
+#         name = "Test"
+#         self.name = "Class A"
+
+#     @classmethod
+#     def prt_something(cls):
+#         print("Invoked prt_something")
+
+# class B(A):
+#     def __init__(self):
+#         super().__init__()
+#         self.age = 20
+
+# obj = A()
+# A.prt_something()
+
+
+
+
+
 class A:
     def __init__(self):
         name = "Test"
@@ -9,8 +30,14 @@ class A:
 
 class B(A):
     def __init__(self):
-        super().__init__()
         self.age = 20
 
-obj = A()
-A.prt_something()
+class C(B):
+    def __init__(self):
+        self.nick = "Class C"  
+
+obj = C()
+
+print(obj.nick) # class C
+print(obj.age)
+print(obj.name)
