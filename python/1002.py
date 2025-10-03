d6dbabb1a193d9f58fcc9ed9963a8c961b8968a9
mj99k14 <ekfkawnl644@gmail.com>
2025-10-03 22:05:32 +0900

    1003

--- python/1002.py
+++ python/1002.py
@@ -40,14 +40,49 @@ from typing import Union
 
 
 
-def bar(x:int):
-    print("x")
-def bar(y:float):
-    print("y")
-def bar(z: str):
-    print("z")
-bar(3)
+# def bar(x:int):
+#     print("x")
+# def bar(y:float):
+#     print("y")
+# def bar(z: str):
+#     print("z")
+# bar(3)
+
+# from functools import singledispatch, singledispatchmethod
+
+# # 매개변수 1개 -> 오버로딩 구현
+# # 지원 자료형은 int, float
+
+# @singledispatch
+# def bar(x):
+#     raise TypeError("unsupported type")
+
+# @bar.register(float)
+# def _(x : float):
+#     print("float")
+    
+# @bar.register(int)
+# def _(x : int):
+#     print("int")
+
+# bar(2.0)
 
 
+from abc import ABC,abstractmethod
+# 추상 클래스를 정의
+#java -> abstract class Bar{}
+
+#Bar를 추상 클래스로 정의
+class Bar(ABC):
+    #추상 인스턴스 매서드 정의
+    @abstractmethod
+    def instance_method(self):
+        pass
 
 
+class MyClass(Bar):
+    def instance_method(self):
+        print("MyClass : instance_method")
+
+obj = MyClass()
+obj = Bar()
