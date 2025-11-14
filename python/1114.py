f64e240c8163e81e11ded960fb0ab44691f42f0d
kimminjung <ekfkawnl644@gmail.com>
2025-11-14 17:27:20 +0900

    Decorator

--- python/1114.py
+++ python/1114.py
@@ -48,29 +48,59 @@
 #     print("bar")
     
     
-def strip(func):
-    def wrapper(msg:str):
-        func( msg.strip())
-    return wrapper
+# def strip(func):
+#     def wrapper(msg:str):
+#         func( msg.strip())
+#     return wrapper
 
-def upper(func):
-    def wrapper(msg:str):
-        func( msg.upper())
-    return wrapper
+# def upper(func):
+#     def wrapper(msg:str):
+#         func( msg.upper())
+#     return wrapper
 
-@upper
-def prt_something1(msg:str):
-    print(f"prt1:{msg}")
+# @upper
+# def prt_something1(msg:str):
+#     print(f"prt1:{msg}")
     
-@strip
-def prt_something2(msg:str):
-    print(f"prt2:{msg}")
+# @strip
+# def prt_something2(msg:str):
+#     print(f"prt2:{msg}")
 
-@upper
-@strip 
-def prt_something3(msg:str):
-    print(f"prt3:{msg}")
+# @upper
+# @strip 
+# def prt_something3(msg:str):
+#     print(f"prt3:{msg}")
     
-prt_something1("        test1         ")
-prt_something2("        test2         ")
-prt_something3("        test3         ")
+# prt_something1("        test1         ")
+# prt_something2("        test2         ")
+# prt_something3("        test3         ")
+
+# from  functools import wraps
+# def upper(func):
+#     @wraps(func)
+#     def wrapper(msg:str):
+#         return func(msg.upper())
+#     return wrapper
+
+
+# @upper
+# def bar(msg: str):
+#   return msg
+# # print(bar("hello"))
+# # print(bar.__name__)
+
+from functools import wraps
+
+def test(n):
+    def factory(func):
+        # route_map['path'] = func
+        def wrapper(func):
+            func()
+        return wrapper
+
+    return factory
+
+@test(1)
+def bar():
+  ...
+bar()
